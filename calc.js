bd7caaef38748d2380493571e09fb018dd8aacef
AvivTarkay <61126933+AvivTatkay@users.noreply.github.com>
2021-01-11 16:17:25 +0200

    added all new

---
+++ calc.js
@@ -0,0 +1,52 @@
+
+
+gridContainer.innerHTML  =  `
+<input id="calcScreen" type="text" placeholder="0" disabled>
+<input id="calcScreen2" type="text"  disabled>
+<button id="plus">+</button><button id="minus">-</button>
+<button id="multi" data-action="multiply">*</button> 
+<button id="division">/</button><button class="number">7</button> 
+<button class="number">8</button><button class="number">9</button>
+<button class="number">4</button><button class="number">5</button>
+<button class="number">6</button><button class="number">1</button> 
+<button class="number">2</button><button class="number">3</button>
+<button class="number">0</button><button id="dot">.</button>
+<button id="ac">C</button><button id="space">=</button>   
+` 
+      let btns = document.getElementsByTagName("button");
+      let calculator = document.getElementById("calcScreen");
+      let calculator2 = document.getElementById("calcScreen2");   
+    
+      for (const caclBtn of btns) {
+        caclBtn.addEventListener('click',function() {
+           if (this.innerHTML == "="){
+            calculator.value = eval(calculator.value); 
+          } 
+           else if (this.innerHTML == "C"){
+            calculator.value = "";
+              }
+            else{
+                calculator.value += this.innerHTML;
+             }     
+        })
+        caclBtn.addEventListener("click",btns.onclick=()=>{
+          calculator2.value = eval(calculator.value);
+        })
+      }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
